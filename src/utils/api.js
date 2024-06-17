7303ed911896d17e373e26ad782b47a55e4922cd
KrtiinSharma <149558605+KrtiinSharma@users.noreply.github.com>
2024-06-17 14:47:25 +0530

    Update api.js
    changed the api key secret information

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-    key: "AIzaSyB2dohVFn1Z_MuaAfVjpyWCLM1_24C3DrQ",
-    cx: "c4b496214a3ea433b",
+    key: "YOUR_API_KEY",
+    cx: "YOUR_cx",
 };
 
 export const fetchDataFromApi = async (payload) => {
